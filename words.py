02e1d11471def22c8259880111b015e64a3eb598
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-27 12:07:13 -0800

    only prints starting with e

--- words.py
+++ words.py
@@ -1,14 +1,16 @@
 """ words printing for python syntax exercise """
 
-words = ["hello", "hey", "goodbye", "yo", "yes"]
+words = ["hello", "hey", "goodbye", "eagle", "E", "yo", "yes"]
 
 
 
 def print_upper_words(words):
-    """print upper case strings
+    """print upper case strings if str starts with e (upper or lowercase)
     input a list of strings. no return"""
 
     for word in words:
-        print(word.upper())
+        word = word.upper()
+        if word.startswith('E'):
+            print(word)
 
 print_upper_words(words)
